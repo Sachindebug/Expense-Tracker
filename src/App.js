af3b774c24fcdfd352549d46eadb8abc06a9d0f8
Sachindebug <st19138914@gmail.com>
2022-05-28 11:14:36 +0530

    testing

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { useState } from "react";
 const INITIAL_EXPENSE = [
   {
     id: 'e1',
-    title: 'Toilet Paper',
+    title: 'Laptop',
     amount: 94.12,
     date: new Date(2020, 7, 14),
   },
